7bbe0ffb8e077daed4bb6501a27af78a788c7e58
Bernie Elbourn <benri421@gmail.com>
2025-07-16 20:28:18 +0100

    v1.0.1 API 36 updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,15 @@ plugins {
 
 android {
     namespace = "com.elbourn.android.wifisetup"
-    compileSdk = 33
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.elbourn.android.wifisetup"
         minSdk = 24
-        targetSdk = 33
-        versionCode = 2
-        versionName = "1.0"
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 36
+        versionCode = 3
+        versionName = "v1.0.1 API 36 updates"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -37,12 +38,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
